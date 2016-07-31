212dff31cfa9c46ab1d38d177cf374459aa2609b
Amanda Hoffman <amandah@alumni.rutgers.edu>
2016-07-30 21:55:52 -0400

    adding survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,57 +7,53 @@ var friends = [
   {
     name: "Animal",
     photo: "http://vignette3.wikia.nocookie.net/muppet/images/7/71/Animaldrums.png",
-    scores: [5, 1, 4, 4, 5, 1, 2, 4, 1, 3]
+    scores: [2, 1, 5, 2, 3, 1, 2, 2, 1, 3]
   },
    {
     name: "Bert",
     photo: "http://vignette2.wikia.nocookie.net/muppet/images/4/44/Bert_pinata.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 4, 1]
+    scores: [2, 1, 2, 3, 5, 1, 2, 4, 1, 2]
   },
    {
     name: "Ernie",
     photo: "http://vignette1.wikia.nocookie.net/muppet/images/a/ad/Ernie%26RubberDuckie-2009.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 4, 1]
+    scores: [2, 1, 4, 4, 3, 1, 2, 4, 1, 5]
   },
    {
     name: "Big Bird",
     photo: "http://vignette1.wikia.nocookie.net/muppet/images/b/b0/BigBirdsSummerGear.png",
-    scores: [5, 1, 4, 4, 5, 1, 2, 4, 1]
+    scores: [2, 1, 4, 4, 3, 1, 5, 4, 1, 2]
   },
    {
     name: "Beaker",
     photo: "http://vignette1.wikia.nocookie.net/muppet/images/1/13/Beakie-%282011%29.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 4, 1]
+    scores: [2, 1, 2, 5, 3, 1, 2, 4, 1, 3]
   },
    {
     name: "Cookie Monster",
     photo: "http://vignette3.wikia.nocookie.net/muppet/images/2/20/41591_54067236007_3060927_n.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 4, 1]
+    scores: [5, 4, 3, 2, 1, 2, 2, 4, 1, 2]
   },
    {
     name: "Count",
     photo: "http://vignette4.wikia.nocookie.net/muppet/images/7/7b/Count-3.png",
-    scores: [5, 1, 4, 4, 5, 1, 2, 4, 1]
+    scores: [2, 1, 2, 3, 4, 5, 4, 3, 1, 2]
   },
-   {
-    name: "Wembley Fraggle",
-    photo: "http://vignette2.wikia.nocookie.net/muppet/images/a/a8/Wembley_Fraggle.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 4, 1]
-  },
+ 
    {
     name: "Grover",
     photo: "http://vignette1.wikia.nocookie.net/muppet/images/a/ad/Coco_OK.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 4, 1]
+    scores: [4, 1, 4, 4, 2, 1, 2, 5, 1, 3]
   },
    {
     name: "Kermit",
     photo: "http://vignette1.wikia.nocookie.net/muppet/images/0/0e/KermitGrin.png",
-    scores: [5, 1, 4, 4, 5, 1, 2, 4, 1]
+    scores: [2, 1, 4, 4, 5, 1, 2, 4, 5, 2]
   },
    {
     name: "Oscar the Grouch",
     photo: "http://vignette1.wikia.nocookie.net/muppet/images/c/cf/Oscar_Neck_Only.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 4, 1]
+    scores: [1, 5, 1, 1, 1, 1, 1, 1, 1, 1]
   },
 
 ];
